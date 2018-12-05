64abaae616e731612de052692d6b5a42b1dd1d4e
Bertrand KHE <bertrand@it-consultis.com>
2018-12-05 16:53:19 +0800

    caching metatag controller

--- src/Controller/MetatagController.php
+++ src/Controller/MetatagController.php
@@ -9,6 +9,7 @@
 namespace Drupal\itc_jsonapi\Controller;
 
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\itc_jsonapi\CacheableJsonApiResponse;
@@ -29,40 +30,52 @@ class MetatagController {
    */
   protected $metatagManager;
 
+  /**
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
+
   public function __construct() {
     $this->entityRepository = \Drupal::service('entity.repository');
     $this->languageManager = \Drupal::languageManager();
     $this->metatagManager = \Drupal::service('metatag.manager');
+    $this->cache = \Drupal::cache();
   }
 
   public function metatag(Request $request) {
     $entity_type = $request->query->get('entity_type');
     $uuid = $request->query->get('id');
-    if (!in_array($entity_type, self::ALLOWED_ENTITY_TYPES)) {
-      return new JsonApiResponse();
-    }
-    if (!is_string($uuid)) {
-      return new JsonApiResponse();
-    }
-    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-    $entity = $this->entityRepository->loadEntityByUuid($entity_type, $uuid);
-    if (empty($entity)) {
-      return new JsonApiResponse();
-    }
     $language = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT);
     $langcode = $language->getId();
-    if ($entity->hasTranslation($langcode)) {
-      $entity = $entity->getTranslation($langcode);
+    $cache_key = "${entity_type}:${uuid}:${langcode}";
+    $cache_item = $this->cache->get($cache_key);
+    if (empty($cache_item)) {
+      if (!in_array($entity_type, self::ALLOWED_ENTITY_TYPES)) {
+        return new JsonApiResponse();
+      }
+      if (!is_string($uuid)) {
+        return new JsonApiResponse();
+      }
+      /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+      $entity = $this->entityRepository->loadEntityByUuid($entity_type, $uuid);
+      if (empty($entity)) {
+        return new JsonApiResponse();
+      }
+      if ($entity->hasTranslation($langcode)) {
+        $entity = $entity->getTranslation($langcode);
+      }
+      $tags = $this->metatagManager->tagsFromEntityWithDefaults($entity);
+      $metatags = $this->metatagManager->generateElements($tags, $entity)['#attached']['html_head'];
+      $data = [
+        'data' => [],
+      ];
+      foreach ($metatags as $tag) {
+        $data['data'][] = $tag[0];
+      }
+      $this->cache->set($cache_key, $data, Cache::PERMANENT, $entity->getCacheTags());
+      $cache_item = $this->cache->get($cache_key);
     }
-    $tags = $this->metatagManager->tagsFromEntityWithDefaults($entity);
-    $metatags = $this->metatagManager->generateElements($tags, $entity)['#attached']['html_head'];
-    $data = [
-      'data' => [],
-    ];
-    foreach ($metatags as $tag) {
-      $data['data'][] = $tag[0];
-    }
-    $response = new JsonApiResponse(json_encode($data));
+    $response = new JsonApiResponse(json_encode($cache_item->data['data']));
     return $response;
   }
 }
