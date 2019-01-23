366c5373646dfa2d859dc4f0fce393c2138fe064
Bertrand KHE <bertrand@it-consultis.com>
2019-01-23 13:46:20 +0800

    Remove alias resolver, file controller. Fix home path for CanonicalLinkToAliasEnhancer

--- src/Plugin/jsonapi/FieldEnhancer/CanonicalLinkToAliasEnhancer.php
+++ src/Plugin/jsonapi/FieldEnhancer/CanonicalLinkToAliasEnhancer.php
@@ -8,6 +8,7 @@
 
 namespace Drupal\itc_jsonapi\Plugin\jsonapi\FieldEnhancer;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
@@ -36,6 +37,13 @@ class CanonicalLinkToAliasEnhancer extends ResourceFieldEnhancerBase implements 
   protected $languageManager;
 
   /**
+   * Service config.factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
@@ -43,18 +51,20 @@ class CanonicalLinkToAliasEnhancer extends ResourceFieldEnhancerBase implements 
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('language_manager')
+      $container->get('language_manager'),
+      $container->get('config.factory')
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $language_manager) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LanguageManagerInterface $language_manager, ConfigFactoryInterface $config_factory) {
     $this->configuration = $configuration;
     $this->pluginId = $plugin_id;
     $this->pluginDefinition = $plugin_definition;
     $this->languageManager = $language_manager;
+    $this->configFactory = $config_factory;
   }
 
 
@@ -65,6 +75,17 @@ class CanonicalLinkToAliasEnhancer extends ResourceFieldEnhancerBase implements 
     return [];
   }
 
+  protected function isHomePath($path, LanguageInterface $language) {
+    $home_path = $this->configFactory->get('system.site')->get('page.front');
+    if($path === $home_path) {
+      return TRUE;
+    }
+    $home_url = Url::fromUserInput($home_path, ['language' => $language])
+      ->toString(TRUE)
+      ->getGeneratedUrl();
+    return $home_url === $path;
+  }
+
   protected function doUndoTransform($data, Context $context) {
     $language = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT);
     $uri = $data['uri'];
@@ -72,8 +93,12 @@ class CanonicalLinkToAliasEnhancer extends ResourceFieldEnhancerBase implements 
       'language' => $language,
     ]);
     $next_value = $data;
-    $next_value['uri'] = $url->toString(TRUE)->getGeneratedUrl();;
-
+    $next_value['url'] = $url->toString(TRUE)->getGeneratedUrl();
+    if ($this->isHomePath($next_value['url'], $language)) {
+      $next_value['url'] = Url::fromRoute('<front>', [], ['language' => $language])
+        ->toString(TRUE)
+        ->getGeneratedUrl();
+    }
     return $next_value;
   }
 
