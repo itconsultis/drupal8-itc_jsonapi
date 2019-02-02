fc85f87792b52e5740d9550fad164a1584db909e
Bertrand KHE <bertrand@it-consultis.com>
2019-02-02 13:47:22 +0800

    Caching for metatag controller

--- src/Controller/MetatagController.php
+++ src/Controller/MetatagController.php
@@ -12,6 +12,7 @@ namespace Drupal\itc_jsonapi\Controller;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Render\RenderContext;
 use Drupal\Core\Url;
 use Drupal\itc_jsonapi\CacheableJsonApiResponse;
 use Drupal\itc_jsonapi\JsonApiResponse;
@@ -32,15 +33,15 @@ class MetatagController {
   protected $metatagManager;
 
   /**
-   * @var \Drupal\Core\Cache\CacheBackendInterface
+   * @var \Drupal\Core\Render\Renderer
    */
-  protected $cache;
+  protected $renderer;
 
   public function __construct() {
     $this->entityRepository = \Drupal::service('entity.repository');
     $this->languageManager = \Drupal::languageManager();
     $this->metatagManager = \Drupal::service('metatag.manager');
-    $this->cache = \Drupal::cache();
+    $this->renderer = \Drupal::service('renderer');
     $this->configFactory = \Drupal::configFactory();
   }
 
@@ -60,52 +61,52 @@ class MetatagController {
     $uuid = $request->query->get('id');
     $language = $this->languageManager->getCurrentLanguage(LanguageInterface::TYPE_CONTENT);
     $langcode = $language->getId();
-    $cache_key = "${entity_type}:${uuid}:${langcode}";
-    $cache_item = $this->cache->get($cache_key);
-    if (empty($cache_item)) {
-      if (!in_array($entity_type, self::ALLOWED_ENTITY_TYPES)) {
-        return new JsonApiResponse('', 400);
-      }
-      if (!is_string($uuid)) {
-        return new JsonApiResponse('', 400);
-      }
-      /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-      $entity = $this->entityRepository->loadEntityByUuid($entity_type, $uuid);
-      if (empty($entity)) {
-        return new JsonApiResponse('', 404);
-      }
-      if ($entity->hasTranslation($langcode)) {
-        $entity = $entity->getTranslation($langcode);
-      }
-      $tags = $this->metatagManager->tagsFromEntityWithDefaults($entity);
-      $metatags = $this->metatagManager->generateElements($tags, $entity)['#attached']['html_head'];
-      $data = [
-        'data' => [],
-      ];
-      foreach ($metatags as $tag) {
-        [$tag_data, $tag_name] = $tag;
-        switch ($tag_name) {
-          case 'canonical_url':
-            $path = parse_url($tag_data['#attributes']['href'], PHP_URL_PATH);
-            $tag_data['#attributes']['href'] = $path;
-            if ($this->isHomePath($path, $language)) {
-              $home_url = Url::fromRoute('<front>', [], ['language' => $language])
-                ->toString(TRUE)
-                ->getGeneratedUrl();
-              $tag_data['#attributes']['href'] = $home_url;
-            }
-            $data['data'][] = $tag_data;
-            break;
-
-          default:
-            $data['data'][] = $tag_data;
-            break;
-        }
-      }
-      $this->cache->set($cache_key, $data, Cache::PERMANENT, $entity->getCacheTags());
-      $cache_item = $this->cache->get($cache_key);
+    if (!in_array($entity_type, self::ALLOWED_ENTITY_TYPES)) {
+      return new JsonApiResponse('', 400);
     }
-    $response = new JsonApiResponse(json_encode($cache_item->data));
+    if (!is_string($uuid)) {
+      return new JsonApiResponse('', 400);
+    }
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $entity = $this->entityRepository->loadEntityByUuid($entity_type, $uuid);
+    if (empty($entity)) {
+      return new JsonApiResponse('', 404);
+    }
+    if ($entity->hasTranslation($langcode)) {
+      $entity = $entity->getTranslation($langcode);
+    }
+    $tags = $this->metatagManager->tagsFromEntityWithDefaults($entity);
+    $context = new RenderContext();
+    $metatags = $this->renderer->executeInRenderContext($context, function () use ($tags, $entity) {
+      return $this->metatagManager->generateElements($tags, $entity)['#attached']['html_head'];
+    });
+    $data = [
+      'data' => [],
+    ];
+    foreach ($metatags as $tag) {
+      [$tag_data, $tag_name] = $tag;
+      switch ($tag_name) {
+        case 'canonical_url':
+          $path = parse_url($tag_data['#attributes']['href'], PHP_URL_PATH);
+          $tag_data['#attributes']['href'] = $path;
+          if ($this->isHomePath($path, $language)) {
+            $home_url = Url::fromRoute('<front>', [], ['language' => $language])
+              ->toString(TRUE)
+              ->getGeneratedUrl();
+            $tag_data['#attributes']['href'] = $home_url;
+          }
+          $data['data'][] = $tag_data;
+          break;
+
+        default:
+          $data['data'][] = $tag_data;
+          break;
+      }
+    }
+    $response = new CacheableJsonApiResponse(json_encode($data));
+    if (!empty($context->isEmpty())) {
+      $response->addCacheableDependency($context->pop());
+    }
     return $response;
   }
 }
