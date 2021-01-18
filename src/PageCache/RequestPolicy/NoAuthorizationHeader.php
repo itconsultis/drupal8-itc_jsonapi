46e92a80d27bf674a7b02a842d5504e166a7156f
Bertrand KHE <bertrand.khe@it-consultis.com>
2021-01-18 10:44:46 +0800

    Disable page cache if request contains authorization header

---
+++ src/PageCache/RequestPolicy/NoAuthorizationHeader.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Drupal\itc_jsonapi\PageCache\RequestPolicy;
+
+
+use Drupal\Core\PageCache\RequestPolicyInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * A policy allowing delivery of cached pages when there is no authorization token.
+ *
+ * Do not serve cached pages to authenticated users.
+ */
+class NoAuthorizationHeader implements RequestPolicyInterface {
+
+  public function check(Request $request) {
+    return $request->headers->has('authorization')
+      ? NoAuthorizationHeader::DENY
+      : NoAuthorizationHeader::ALLOW;
+  }
+
+}
